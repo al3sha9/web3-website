891cfce73bb72a48b2c70434a4f6777d72cceb2f
alishan <optimist.alishan@gmail.com>
2023-07-27 23:38:44 +0500

    push

--- src/components/imagine.jsx
+++ src/components/imagine.jsx
@@ -1,11 +1,16 @@
-export default function Imagine(){
+export default function Imagine() {
 
-    return(
+    return (
         <section className="imagine">
             <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <h2>IMAGINE A WORLD WHERE...</h2>
+                <div className="mob p-5">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h2>IMAGINE A WORLD WHERE...</h2>
+                        </div>
+                        <div className="col-md-6">
+                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, eius!</h2>
+                        </div>
                     </div>
                 </div>
             </div>
