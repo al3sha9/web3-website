417857958915f5b8b67fd119a150d1ea3ba7ba02
alishan <optimist.alishan@gmail.com>
2023-07-31 17:12:36 +0500

    new

--- src/components/imagine.jsx
+++ src/components/imagine.jsx
@@ -7,19 +7,29 @@ export default function Imagine() {
                     <div className="mob pt-5 ps-5 pe-5">
                         <div className="row">
                             <div className="col-md-6">
-                                <h2>IMAGINE A WORLD WHERE...</h2>
+                                <h2 className="imagine-h2">IMAGINE A WORLD WHERE...</h2>
                             </div>
                             <div className="col-md-6">
-                                <p><span>Blockspace Offering:</span> Most Enterprises Are Not Concerned With Specific Blockchains, But Rather Technical Requirements For Their Use Cases, Like Security, Interoperability And Throughput. Our Product Focuses On Providing.</p>
+                                <p className="mt-2"><span>Blockspace Offering:</span> Their Use Cases, Like Security, Interoperability And Throughput. Our Product Focuses On Providing.</p>
                             </div>
-                            <hr />
+                            <hr /> 
+                            {/* comment */}
                         </div>
                         <div className="row">
                             <div className="col-md-6">
-                                <h2>IMAGINE A WORLD WHERE...</h2>
+                                <div className="row">
+                                    <div className="col-md-5 imagine-para">
+                                        <h3>$4.9B</h3>
+                                        <p>Market size was <br />valued in 2021</p>
+                                    </div>
+                                    <div className="col-md-7 text-start imagine-para">
+                                        <h3>$4.9B</h3>
+                                        <p>Market size was <br />valued in 2021</p>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-6">
-                                <p><span>Blockspace Offering:</span> Most Enterprises Are Not Concerned With Specific Blockchains, But Rather Technical Requirements For Their Use Cases, Like Security, Interoperability And Throughput. Our Product Focuses On Providing.</p>
+                                <p className="mt-2"><span>Blockspace Offering:</span> Their Use Cases, Like Security, Interoperability And Throughput. Our Product Focuses On Providing.</p>
                             </div>
                         </div>
                     </div>
